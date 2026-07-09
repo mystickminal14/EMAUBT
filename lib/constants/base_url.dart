03961b7e4814eba5f720085f7ae475556d708b50
charitra shrestha <stharabi9862187405@gmail.com>
2026-07-09 16:38:33 +0545

    fix(config):

--- lib/constants/base_url.dart
+++ lib/constants/base_url.dart
@@ -1,6 +1,6 @@
 class BaseUrl {
-  static const String baseUrl = 'https://server.theemaeducation.com/api';
-  static const String imageUrl = 'https://server.theemaeducation.com/api/res';
+  static const String baseUrl = 'https://server.emaeducation.com/api';
+  static const String imageUrl = 'https://server.emaeducation.com/api/res';
 
   // static const String baseUrl = 'http://10.10.100.32:8000/api';
   // static const String imageUrl = 'http://10.10.100.32:8000/api/res';
