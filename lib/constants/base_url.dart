cb010fe7d1eaafe2f658010eaf078d1fc1080d0a
charitra shrestha <stharabi9862187405@gmail.com>
2026-08-02 10:29:13 +0545

    chore(constants): Update local development base URLs

--- lib/constants/base_url.dart
+++ lib/constants/base_url.dart
@@ -2,6 +2,6 @@ class BaseUrl {
   static const String baseUrl = 'https://server.emaeducation.edu.np/api';
   static const String imageUrl = 'https://server.emaeducation.edu.np/api/res';
 
-  // static const String baseUrl = 'http://10.10.100.160:8000/api';
-  // static const String imageUrl = 'http://10.10.100.160:8000/api/res';
+  // static const String baseUrl = 'http://172.16.4.215:8000/api';
+  // static const String imageUrl = 'http://172.16.4.215:8000/api/res';
 }
